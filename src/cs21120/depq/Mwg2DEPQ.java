3b72e0d96ab351debf2c8c26a5c59f95c601b777
MichalGoly <m.goly@goly2.com>
2015-11-08 12:21:50 +0000

    new approach

--- src/cs21120/depq/Mwg2DEPQ.java
+++ src/cs21120/depq/Mwg2DEPQ.java
@@ -7,58 +7,29 @@ package cs21120.depq;
 public class Mwg2DEPQ implements DEPQ {
    
    private Interval[] intervalHeap;
-   private int heapSize;
-   private int elementsNumber;
+   private int size;
    
    public Mwg2DEPQ() {
       intervalHeap = new Interval[500];
-      heapSize = 0;
-      elementsNumber = 0;
+      size = 0;
    }
    
    @Override
    public Comparable inspectLeast() {
-      if (elementsNumber == 0) {
-         return null;
-      } else if (elementsNumber == 1) {
-         Interval root = intervalHeap[0];
-         
-         if (root.leftNode == null) {
-            return root.rightNode;
-         } else {
-            return root.leftNode;
-         }
-      } else {
-         Interval root = intervalHeap[0];
-         return root.leftNode;
-      } 
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
    }
 
    @Override
    public Comparable inspectMost() {
-      if (elementsNumber == 0) {
-         return null;
-      } else if (elementsNumber == 1) {
-         Interval root = intervalHeap[0];
-         
-         if (root.leftNode == null) {
-            return root.rightNode;
-         } else {
-            return root.leftNode;
-         } 
-      } else {
-         Interval root = intervalHeap[0];
-         return root.rightNode;
-      }
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
    }
 
    @Override
    public void add(Comparable c) {
-      if (heapSize == 0) {
-         intervalHeap[0] = new Interval(c, c);
-         heapSize++;
-         elementsNumber++;
-      } 
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
    }
 
    @Override
@@ -73,59 +44,20 @@ public class Mwg2DEPQ implements DEPQ {
 
    @Override
    public boolean isEmpty() {
-      return elementsNumber == 0;
+      return size == 0;
    }
 
    @Override
    public int size() {
-      return elementsNumber;
+      return size;
    }
    
    private class Interval {
       
-      private Comparable leftNode;
-      private Comparable rightNode;
+      private Comparable left;
+      private Comparable right;
       
-      public Interval() {}
-      
-      public Interval(Comparable leftNode, Comparable rightNode) {
-         this.leftNode = leftNode;
-         this.rightNode = rightNode;
-         
-         if (leftNode == null) {
-            this.leftNode = rightNode;
-         }
-         if (rightNode == null) {
-            this.rightNode = leftNode;
-         }
-      }
-
-      public void setLeftNode(Comparable leftNode) {
-         this.leftNode = leftNode;
-         
-         if (this.rightNode == null) {
-            this.rightNode = leftNode;
-         }
-      }
-
-      public void setRightNode(Comparable rightNode) {
-         this.rightNode = rightNode;
-         
-         if (this.leftNode == null) {
-            this.leftNode = rightNode;
-         }
-      }
-
-      public Comparable getLeftNode() {
-         Comparable temp = leftNode;
-         leftNode = null;
-         return temp;
-      }
-
-      public Comparable getRightNode() {
-         Comparable temp = rightNode;
-         rightNode = null;
-         return temp;
+      public Interval() {
       }
       
    }
