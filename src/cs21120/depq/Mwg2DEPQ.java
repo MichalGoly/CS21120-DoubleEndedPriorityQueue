1891dc10fdd09e8cd5cf54e16482cdd8fbc6fbc0
MichalGoly <m.goly@goly2.com>
2015-11-16 22:25:42 +0000

    initial version of the class javadoc comment

--- src/cs21120/depq/Mwg2DEPQ.java
+++ src/cs21120/depq/Mwg2DEPQ.java
@@ -3,9 +3,55 @@ package cs21120.depq;
 /**
  * Mwg2DEPQ is an implementation of the double ended priority queue (DEPQ) interface
  * provided as part of the CS21100 (Program Design, Data Structures And Algorithms)
- * module assignment at Aberystwyth University.
+ * assignment at Aberystwyth University.
  *
  * DEPQ has been implemented using an interval heap which makes it very efficient.
+ * Interval heap is similar to a traditional heap, however each node, represented
+ * by the Interval inner class, contains two Comparable elements. Left element is
+ * typically smaller (can be equal to) than the right one. If the total amount of
+ * elements in the interval heap is odd, the last Interval will hold a second 
+ * 'dummy' element to make the implementation simpler. All left elements in the
+ * interval heap define a min heap, whereas all right elements define a max heap.
+ * Therefore in the root Interval the left element is the smallest one in the heap
+ * and the right one is the largest one in the heap. Internally all elements are
+ * stored inside a standard array which can be expanded if necessary. We also keep
+ * track of the size using an instance variable.
+ * 
+ * TIME COMPLEXITY SUMMARY
+ * 
+ * Each significant method has an associated complexity analysis in the JavaDoc,
+ * however the summary of the efficiency of all methods is presented below.
+ * 
+ * Method name    | Complexity
+ * ---------------+---------------
+ * size()         | O(1)
+ * isEmpty()      | O(1)
+ * inspectLeast() | O(1)
+ * inspectMost()  | O(1)
+ * add()          | O(log n)
+ * getLeast()     | O(log n)
+ * getMost()      | O(log n)
+ * 
+ * SOURCES
+ * 
+ * In order to understand the concept of an interval heap and the way to implement
+ * it, I extensively used this web site:
+ * http://www.cise.ufl.edu/~sahni/dsaaj/enrich/c13/double.htm
+ * 
+ * In order to see how other developers tackled the problem I read through this 
+ * web site:
+ * http://www.mhhe.com/engcs/compsci/sahni/enrich/c9/interval.pdf
+ * 
+ * SELF EVALUATION
+ * 
+ * I belive I have met all the marking criteria to receive a mark of around 80%
+ * in each part of the assessment. My DEPQ is functioning correctly, has a very
+ * high efficiency due to the way it has been implemented. Furthermore, the 
+ * documentation is clear an informative and each significant method has an 
+ * associated complexity analysis with the summary in the class-wide JavaDoc 
+ * comment. Finally, I believe that my code is clean an simple to understand, which
+ * means that I would hope to receive an overall mark of around 80% for this 
+ * assignment. 
  *
  * @author Michal Goly, mwg2@aber.ac.uk
  */
